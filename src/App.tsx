07bec61172fd73e00ef8d66a7b0f4e21c9a3d0e1
Aleksei <48284352+nicealx@users.noreply.github.com>
2024-05-01 20:54:44 +0700

    Add React on project (feat: CUSTOM-ECOMM-2) (#28)
    * feat: add react and react-dom, add support react on tsconfig, add base files, add declaration style modules
    * feat: add material ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => (
+  <section>
+    <div className={styles.App}>
+      <h1 className={styles.App__title}>React TypeScript Webpack Starter Template</h1>
+      <p className={styles.App__text}>Text</p>
+    </div>
+  </section>
+);
+export default App;

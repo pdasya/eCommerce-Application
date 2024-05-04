c636848620b8a97f308efaee8b96db19dd8c2aa5
jasper7466 <57725156+jasper7466@users.noreply.github.com>
2024-05-04 18:56:15 +0500

    Set up folder structure (refactor: RSS-ECOMM-1_02) (#54)
    * refactor: add basic top-level fsd folder structure
    
    * refactor: add more detailed fsd folder structure
    
    * chore: add fsd path aliases

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import App from './App';
+import App from './app/app.component';
 
 const root = document.createElement('div');
 root.setAttribute('id', 'root');

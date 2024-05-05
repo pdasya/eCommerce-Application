f309d4eb04037fde72333ea98b96a16c4df7bc6f
jasper7466 <57725156+jasper7466@users.noreply.github.com>
2024-05-05 10:48:50 +0500

    Install and configure Jest (chore: RSS-ECOMM-1_13) (#55)
    * chore: add and configure jest
    
    * feat: add dummy entity for testing
    
    * test: add dummy tests
    
    * test: fix dummy test
    
    * chore: apply prettier
    
    * chore: remove temporary script
    
    * chore: fix dev-dependencies

---
+++ src/entities/dummy/ui/dummy.component.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Dummy } from '..';
+
+describe('dummy component', () => {
+  let rendered: ReturnType<typeof render>;
+
+  beforeEach(() => {
+    rendered = render(<Dummy />);
+  });
+
+  test("renders heading with 'Some header' text content", () => {
+    expect(screen.getByRole('heading', { name: 'Some header' })).toBeInTheDocument();
+  });
+
+  test('renders only 1 paragraph', () => {
+    const paragraphs = screen.getAllByRole('paragraph');
+    expect(paragraphs.length).toBe(1);
+  });
+
+  test("contains at least 1 item with 'Some text'", () => {
+    const textItems = screen.getAllByText('Some text');
+    const minLength = 1;
+
+    expect(textItems.length).toBeGreaterThanOrEqual(minLength);
+  });
+
+  test('snapshot test', () => {
+    expect(rendered.asFragment()).toMatchSnapshot('dummy');
+  });
+});

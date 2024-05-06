bc4a9048ba6f78c0df16d6f386cb75b24f199cfd
nicealx <kav.prok@gmail.com>
2024-05-06 11:50:38 +0700

    feat: add constants.ts and add environment constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+export const projectKey = process.env.CTP_PROJECT_KEY;
+export const clientSecret = process.env.CTP_CLIENT_SECRET;
+export const clientId = process.env.CTP_CLIENT_ID;
+export const authUrl = process.env.CTP_AUTH_URL;
+export const apiUrl = process.env.CTP_API_URL;
+export const scopes = process.env.CTP_SCOPES;

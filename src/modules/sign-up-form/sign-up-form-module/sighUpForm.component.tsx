071cfde6e64fc4c374b48d63c121571d784c2c4a
Pashkova Daria <dar.pashcovaa@gmail.com>
2024-05-08 15:49:42 +0900

    feat: apply linters for sign-up-form module

---
+++ src/modules/sign-up-form/sign-up-form-module/sighUpForm.component.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import styles from './sighUpForm.component.module';
+
+export const Dummy: FC = () => (
+  <section>
+    <div className={styles.Dummy}>
+      <h1>Some header</h1>
+      <p>Some text</p>
+    </div>
+  </section>
+);

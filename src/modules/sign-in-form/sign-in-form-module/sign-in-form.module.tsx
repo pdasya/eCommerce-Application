69b54f509c308bf1b8bbe0a0acb98924654b4caa
nicealx <kav.prok@gmail.com>
2024-05-10 00:05:03 +0700

    refactor: add password validation rules

--- src/modules/sign-in-form/sign-in-form-module/sign-in-form.module.tsx
+++ src/modules/sign-in-form/sign-in-form-module/sign-in-form.module.tsx
@@ -1,26 +1,27 @@
 import React, { FC } from 'react';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 import { FormTemplate } from '../../../components/input-form/input-form.component';
 
 /* eslint-disable max-lines-per-function */
 export const SignInForm: FC = () => {
-  // const minPasswordLength = 8;
+  const minPasswordLength = 8;
 
   const initialValues = {
     email: '',
     password: '',
   };
 
-  // const validationSchema = Yup.object({
-  //   email: Yup.string().email('Invalid email address').required('Email is required'),
-  //   password: Yup.string()
-  //     .min(minPasswordLength, 'Password must be at least 8 characters long')
-  //     .matches(
-  //       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
-  //       'Password must contain at least one uppercase letter, one lowercase letter, and one number',
-  //     )
-  //     .required('Password is required'),
-  // });
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string()
+      .min(minPasswordLength, 'Password must be at least 8 characters long')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+        'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+      )
+      .test('Check password', 'Password must not contain spaces', value => !value?.includes(' '))
+      .required('Password is required'),
+  });
 
   const handleSubmit = (values: Record<string, string>): void => {
     console.log('Form Values:', values);
@@ -49,7 +50,7 @@ export const SignInForm: FC = () => {
       buttonText="Login"
       fields={fields}
       initialValues={initialValues}
-      // validationSchema={validationSchema}
+      validationSchema={validationSchema}
       onSubmit={handleSubmit}
     />
   );

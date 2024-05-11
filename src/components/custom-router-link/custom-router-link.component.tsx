8b0537f4534f2970935db4d1b848c0c4eb7abd6a
Tim <jasper7466@yandex.ru>
2024-05-11 07:31:03 +0500

    feat: implement custom-router-link component

---
+++ src/components/custom-router-link/custom-router-link.component.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import { Link, LinkProps, useMatch, useResolvedPath } from 'react-router-dom';
+import classNames from 'classnames';
+import styles from './custom-router-link.component.module.scss';
+
+interface ICustomRouterLinkProperties extends LinkProps {
+  activeClassName?: string;
+}
+
+const defaultProperties = {
+  activeClassName: '',
+};
+
+// Spread used because it is just wrapper
+// eslint-disable-next-line react/jsx-props-no-spreading
+export const CustomRouterLink: FC = ({
+  children,
+  to,
+  activeClassName = '',
+  ...props
+}: ICustomRouterLinkProperties): React.JSX.Element => {
+  const resolved = useResolvedPath(to);
+  const match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <Link
+      to={to}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      className={classNames(styles.link, props.className, match && activeClassName)}>
+      {children}
+    </Link>
+  );
+};
+
+CustomRouterLink.defaultProps = defaultProperties;

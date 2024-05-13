5317151581826a7c8464bab37d68d2a82d82eddb
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-05-13 11:44:18 +0900

    refactor: create custom component for sign-up form

---
+++ src/modules/sign-up-form/components/sign-up-form-component/sign-up-form-component.tsx
@@ -0,0 +1,134 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { FC } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import {
+  TextField,
+  Button,
+  Paper,
+  Typography,
+  Grid,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  FormHelperText,
+} from '@mui/material';
+import * as Yup from 'yup';
+import PasswordInputComponent from '@/components/password-input-component/password-input-component';
+import styles from './sign-up-form-component.module.scss';
+
+interface IFormField {
+  id: string;
+  name: string;
+  label: string;
+  type: string;
+  required?: boolean;
+  options?: { label: string; value: string }[]; // For select input
+}
+
+interface ISignUpFormComponentProperties {
+  title: string;
+  buttonText: string;
+  fields: IFormField[];
+  initialValues: Record<string, string>;
+  validationSchema: Yup.ObjectSchema<Record<string, string | Date>>;
+  onSubmit: (values: Record<string, string>) => void;
+}
+
+const BoldUppercaseError: FC<{ name: string }> = ({ name }) => (
+  <ErrorMessage
+    name={name}
+    render={msg => (
+      <FormHelperText error className={styles.errorMessage}>
+        {msg}
+      </FormHelperText>
+    )}
+  />
+);
+
+export const SignUpFormComponent: FC<ISignUpFormComponentProperties> = ({
+  title,
+  buttonText,
+  fields,
+  initialValues,
+  validationSchema,
+  onSubmit,
+}) => (
+  <Paper
+    elevation={3}
+    style={{
+      padding: 20,
+      maxWidth: 400,
+      margin: '20px auto',
+      border: '2px solid black',
+    }}>
+    <Typography variant="h2" align="center" gutterBottom>
+      {title}
+    </Typography>
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      {({ errors, touched }) => (
+        <Form>
+          <Grid container spacing={2}>
+            {fields.map(field => (
+              <Grid item xs={12} key={field.id}>
+                {field.type === 'select' ? (
+                  <FormControl fullWidth variant="standard">
+                    <InputLabel required={field.required}>{field.label}</InputLabel>
+                    <Field
+                      as={Select}
+                      name={field.name}
+                      label={field.label}
+                      required={field.required}
+                      error={touched[field.name] && Boolean(errors[field.name])}>
+                      {field.options?.map(option => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </Field>
+                    {touched[field.name] && <BoldUppercaseError name={field.name} />}
+                  </FormControl>
+                ) : field.type === 'password' ? (
+                  <FormControl fullWidth variant="standard">
+                    <Field
+                      as={TextField}
+                      fullWidth
+                      label={field.label}
+                      name={field.name}
+                      type={field.type}
+                      required={field.required}
+                      variant="standard"
+                      error={touched[field.name] && Boolean(errors[field.name])}
+                      helperText={touched[field.name] && <BoldUppercaseError name={field.name} />}
+                      component={PasswordInputComponent}
+                    />
+                  </FormControl>
+                ) : (
+                  <Field
+                    as={TextField}
+                    fullWidth
+                    label={field.label}
+                    name={field.name}
+                    type={field.type}
+                    required={field.required}
+                    variant="standard"
+                    error={touched[field.name] && Boolean(errors[field.name])}
+                    helperText={touched[field.name] && <BoldUppercaseError name={field.name} />}
+                  />
+                )}
+              </Grid>
+            ))}
+          </Grid>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            style={{ marginTop: 20 }}>
+            {buttonText}
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  </Paper>
+);

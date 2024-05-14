39a4a06388bd7277da2194081910f6884ce45ab8
pdasya <104981098+pdasya@users.noreply.github.com>
2024-05-14 19:53:21 +0900

    Implement error handling for failed registration attempts (feat: RSS-ECOMM-2_12) (#118)
    * fix: fix linter errors
    
    * feat: trying to solve try/catch bug
    
    * feat: add server-side error handling

--- src/modules/sign-up-form/sign-up-form-api/sign-up-form-api.ts
+++ src/modules/sign-up-form/sign-up-form-api/sign-up-form-api.ts
@@ -1,17 +1,20 @@
-import { CustomerDraft } from '@commercetools/platform-sdk';
+import { ClientResponse, CustomerDraft, CustomerSignInResult } from '@commercetools/platform-sdk';
 import { apiRoot } from '@/commercetools/client';
 import { storeKey } from '@/config/constants';
 
-export const createCustomerInStore = async (customerDraft: CustomerDraft) => {
+export const createCustomerInStore = async (
+  customerDraft: CustomerDraft,
+): Promise<ClientResponse<CustomerSignInResult>> => {
   try {
     const response = await apiRoot
       .inStoreKeyWithStoreKeyValue({ storeKey })
       .customers()
       .post({ body: customerDraft })
       .execute();
-
-    console.log('Customer created:', response.body);
+    console.log(response.body);
+    return response;
   } catch (error) {
     console.error('Error creating customer:', error);
+    throw error;
   }
 };
--- src/modules/sign-up-form/sign-up-form-module/sign-up-form-module.tsx
+++ src/modules/sign-up-form/sign-up-form-module/sign-up-form-module.tsx
@@ -1,10 +1,10 @@
 import React, { FC, useState } from 'react';
 import * as Yup from 'yup';
 import { CustomerDraft } from '@commercetools/platform-sdk';
+import { toast } from 'react-toastify';
 import { SignUpFormComponent } from '../sign-up-form-component/sign-up-form-component';
 import { minPasswordLength } from '@/config/constants';
 import { createCustomerInStore } from '../sign-up-form-api/sign-up-form-api';
-import styles from './sign-up-form-module.module.scss';
 
 export const SignUpForm: FC = () => {
   const minStreetNameLength = 1;
@@ -57,10 +57,6 @@ export const SignUpForm: FC = () => {
       .required('Country is required'),
   });
 
-  const [registrationStatus, setRegistrationStatus] = useState<{
-    success: boolean;
-    message?: string;
-  }>({ success: false });
   const [formValues, setFormValues] = useState(initialValues);
 
   const handleSubmit = async (values: Record<string, string>): Promise<void> => {
@@ -83,11 +79,11 @@ export const SignUpForm: FC = () => {
     try {
       const response = await createCustomerInStore(customerDraft);
       console.log('Response:', response);
-      setRegistrationStatus({ success: true, message: 'Registration successful' });
       setFormValues(initialValues);
+      toast.success('Customer Successfully Created');
     } catch (error) {
+      toast.error(`${error.message}`);
       console.error('Error creating customer:', error);
-      setRegistrationStatus({ success: false, message: 'Registration failed' });
     }
   };
 
@@ -159,18 +155,13 @@ export const SignUpForm: FC = () => {
   ];
 
   return (
-    <>
-      {registrationStatus.success && (
-        <p className={styles.registrationSuccessMessage}>{registrationStatus.message}</p>
-      )}
-      <SignUpFormComponent
-        title="Sign Up"
-        buttonText="Register"
-        fields={fields}
-        initialValues={formValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      />
-    </>
+    <SignUpFormComponent
+      title="Sign Up"
+      buttonText="Register"
+      fields={fields}
+      initialValues={formValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    />
   );
 };

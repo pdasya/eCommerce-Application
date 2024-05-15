764b592f62145770b1def5745f576f42c056c134
pdasya <104981098+pdasya@users.noreply.github.com>
2024-05-15 18:33:02 +0900

    Allow users to set a default address during registration (feat: RSS-ECOMM-2_14) (#122)
    * feat: add noValidate rule to sign up form
    
    * feat: add default address checkbox
    
    * feat: configure default shipping address
    
    * refactor: move inline styles to separate file

--- src/modules/sign-up-form/sign-up-form-module/sign-up-form-module.tsx
+++ src/modules/sign-up-form/sign-up-form-module/sign-up-form-module.tsx
@@ -20,6 +20,7 @@ export const SignUpForm: FC = () => {
     city: '',
     postalCode: '',
     country: '',
+    setDefaultAddress: false,
   };
 
   const validationSchema = Yup.object({
@@ -60,20 +61,21 @@ export const SignUpForm: FC = () => {
   const [formValues, setFormValues] = useState(initialValues);
 
   const handleSubmit = async (values: Record<string, string>): Promise<void> => {
+    const address = {
+      country: values.country,
+      city: values.city,
+      streetName: values.street,
+      postalCode: values.postalCode,
+    };
+
     const customerDraft: CustomerDraft = {
       email: values.email,
       password: values.password,
       firstName: values.firstName,
       lastName: values.lastName,
       dateOfBirth: values.dateOfBirth,
-      addresses: [
-        {
-          country: values.country,
-          city: values.city,
-          streetName: values.street,
-          postalCode: values.postalCode,
-        },
-      ],
+      addresses: [address],
+      defaultShippingAddress: values.setDefaultAddress ? 0 : undefined,
     };
 
     try {
@@ -151,6 +153,12 @@ export const SignUpForm: FC = () => {
       type: 'select',
       required: true,
       options: [{ label: 'United States', value: 'US' }],
+    },
+    {
+      id: 'setDefaultAddress',
+      name: 'setDefaultAddress',
+      label: 'Set shipping address as default',
+      type: 'switch',
     },
   ];
 

e717b10671fd6a25c9ce9fbd4b41860986639e3a
Aleksei <kav.prok@gmail.com>
2024-05-17 18:04:37 +0700

    refactor: change build-client

--- src/commercetools/build-client.ts
+++ src/commercetools/build-client.ts
@@ -2,35 +2,139 @@ import {
   AnonymousAuthMiddlewareOptions,
   ClientBuilder,
   HttpMiddlewareOptions,
+  PasswordAuthMiddlewareOptions,
+  RefreshAuthMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
 import {
-  projectKey,
-  clientSecret,
-  clientId,
-  authUrl,
-  apiUrl,
-  scopes,
-  tokenCache,
-} from '../config/constants';
+  ByProjectKeyRequestBuilder,
+  createApiBuilderFromCtpClient,
+} from '@commercetools/platform-sdk';
+import { tokenCache, tokenStorage } from '@/config/constants';
+import { IUserDraft } from '@/modules/sign-in-form/interfaces/sign-in-form.interfaces';
 
-const anonymousAuthMiddlewareOptions: AnonymousAuthMiddlewareOptions = {
-  host: authUrl,
-  projectKey,
-  credentials: {
-    clientId,
-    clientSecret,
-  },
-  scopes: [scopes],
-  tokenCache,
-};
+export default class BuildClient {
+  private projectKey: string;
 
-const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: apiUrl,
-};
+  private clientId: string;
 
-export const client = new ClientBuilder()
-  .withProjectKey(projectKey)
-  .withAnonymousSessionFlow(anonymousAuthMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware()
-  .build();
+  private clientSecret: string;
+
+  private authUrl: string;
+
+  private apiUrl: string;
+
+  private scopes: string[];
+
+  private client: ByProjectKeyRequestBuilder;
+
+  constructor(
+    projectKey: string,
+    clientId: string,
+    clientSecret: string,
+    authUrl: string,
+    apiUrl: string,
+    scopes: string[],
+  ) {
+    this.projectKey = projectKey;
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.authUrl = authUrl;
+    this.apiUrl = apiUrl;
+    this.scopes = scopes;
+    this.client = this.setFlow();
+  }
+
+  private setFlow() {
+    const token = tokenStorage.get('token');
+    if (token) {
+      return this.refreshToken(token.refreshToken);
+    }
+    return this.anonymousSession();
+  }
+
+  public anonymousSession() {
+    const httpMiddlewareOptions: HttpMiddlewareOptions = {
+      host: this.apiUrl,
+    };
+    const options: AnonymousAuthMiddlewareOptions = {
+      host: this.authUrl,
+      projectKey: this.projectKey,
+      credentials: {
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+      },
+      scopes: this.scopes,
+    };
+
+    const ctpClient = new ClientBuilder()
+      .withProjectKey(this.projectKey)
+      .withAnonymousSessionFlow(options)
+      .withHttpMiddleware(httpMiddlewareOptions)
+      .withLoggerMiddleware()
+      .build();
+
+    this.client = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+      projectKey: this.projectKey,
+    });
+    return this.client;
+  }
+
+  public passwordSession(userDraft: IUserDraft) {
+    const httpMiddlewareOptions: HttpMiddlewareOptions = {
+      host: this.apiUrl,
+    };
+    const options: PasswordAuthMiddlewareOptions = {
+      host: this.authUrl,
+      projectKey: this.projectKey,
+      credentials: {
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        user: {
+          username: userDraft.email,
+          password: userDraft.password,
+        },
+      },
+      scopes: this.scopes,
+      tokenCache,
+    };
+
+    const ctpClient = new ClientBuilder()
+      .withProjectKey(this.projectKey)
+      .withPasswordFlow(options)
+      .withHttpMiddleware(httpMiddlewareOptions)
+      .withLoggerMiddleware()
+      .build();
+
+    this.client = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+      projectKey: this.projectKey,
+    });
+    return this.client;
+  }
+
+  public refreshToken(refreshToken: string) {
+    const httpMiddlewareOptions: HttpMiddlewareOptions = {
+      host: this.apiUrl,
+    };
+    const options: RefreshAuthMiddlewareOptions = {
+      host: this.authUrl,
+      projectKey: this.projectKey,
+      credentials: {
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+      },
+      refreshToken,
+      tokenCache,
+    };
+
+    const ctpClient = new ClientBuilder()
+      .withProjectKey(this.projectKey)
+      .withRefreshTokenFlow(options)
+      .withHttpMiddleware(httpMiddlewareOptions)
+      .withLoggerMiddleware()
+      .build();
+    this.client = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+      projectKey: this.projectKey,
+    });
+    return this.client;
+  }
+}

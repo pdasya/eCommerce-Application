146308390fd4391df4f84f12d6ad9b21f15332af
jasper7466 <57725156+jasper7466@users.noreply.github.com>
2024-05-19 15:23:49 +0500

    Add some minor improvements (refactor: CUSTOM-ECOMM-30)

--- src/modules/dummy-module/index.ts
+++
@@ -1,1 +0,0 @@
-export { DummyCounter } from './components/dummy-counter/dummy-counter.component';
--- src/pages/main/main.page.tsx
+++ src/pages/main/main.page.tsx
@@ -1,62 +1,35 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
+import { RoutePath } from '@routes/index';
+import { CustomRouterLink } from '@components/custom-router-link/custom-router-link.component';
 import { Button } from '@mui/material';
-import { toast } from 'react-toastify';
-import { useAppDispatch } from '@/hooks/use-app-dispatch.hook';
-import { authorize, unauthorize } from '@/modules/auth/auth.slice';
 import styles from './main.page.module.scss';
-import { tokenName, tokenStorage } from '@/config/constants';
 
 export const MainPage: FC = () => {
-  const dispatch = useAppDispatch();
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    if (isError) {
-      throw new Error('Some error description');
-    }
-  });
+  const navItems = [
+    { caption: 'Home', path: RoutePath.main },
+    { caption: 'Profile (authorized only)', path: RoutePath.profile },
+    { caption: 'Register (unauthorized only)', path: RoutePath.signUp },
+    { caption: 'Sign in (unauthorized only)', path: RoutePath.signIn },
+    { caption: 'Catalog', path: RoutePath.catalog },
+    { caption: 'Product', path: RoutePath.product },
+    { caption: 'Cart', path: RoutePath.cart },
+    { caption: 'About', path: RoutePath.about },
+    { caption: '404', path: RoutePath.http404 },
+    { caption: 'Error', path: RoutePath.error },
+  ];
 
   return (
     <div className={styles.page}>
-      <ul>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            tokenStorage.set(tokenName, {
-              token: 'token',
-              expirationTime: 0,
-              refreshToken: 'refresh token',
-            });
-            dispatch(authorize({ id: 1 }));
-          }}>
-          fake sign in
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => {
-            tokenStorage.clear();
-            dispatch(unauthorize({}));
-          }}>
-          fake sign out
-        </Button>
-        <Button
-          variant="contained"
-          color="warning"
-          onClick={() => {
-            setIsError(true);
-          }}>
-          throw error
-        </Button>
-        <Button
-          variant="contained"
-          color="info"
-          onClick={() => {
-            toast.error('Some error message');
-          }}>
-          toastify
-        </Button>
+      <ul className={styles.list}>
+        {navItems.map(item => (
+          <li key={item.caption}>
+            <CustomRouterLink to={item.path}>
+              <Button variant="outlined" sx={{ color: 'white', borderColor: 'white' }}>
+                {item.caption}
+              </Button>
+            </CustomRouterLink>
+          </li>
+        ))}
       </ul>
     </div>
   );

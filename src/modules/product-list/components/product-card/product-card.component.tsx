dfe5a66c8f5c36e88a730fe352e8fa900b7ca076
Aleksei <48284352+nicealx@users.noreply.github.com>
2024-05-24 10:34:04 +0700

    Fetch and Display Product List using Commercetools API (feat: RSS-ECOMM-3_01)

--- src/modules/product-list/components/product-card/product-card.component.tsx
+++ src/modules/product-list/components/product-card/product-card.component.tsx
@@ -3,43 +3,48 @@ import { Button } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import DiscountIcon from '@mui/icons-material/Discount';
 import classNames from 'classnames';
-import { IProduct } from '@modules/product-list/interfaces/product.interface';
+import { IProduct } from '@/interfaces/interfaces';
 import styles from './product-card.component.module.scss';
 
 type ProductCardProps = IProduct;
 
 export const ProductCard: FC<ProductCardProps> = ({
   imageSrc,
+  imageAlt,
   title,
   description,
-  price,
+  currentPrice,
   currency,
   discountPrice,
 }) => (
   <div className={styles.root}>
-    {discountPrice ? <DiscountIcon className={styles.iconDiscount} /> : ''}
-    <div className={styles.imageContainer}>
-      <img className={styles.image} src={imageSrc} alt="product" />
+    <div className={styles.wrap}>
+      {discountPrice ? <DiscountIcon className={styles.iconDiscount} /> : ''}
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={imageSrc} alt={imageAlt} />
+      </div>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.description}>{description}</p>
+      <div className={styles.bottomContainer}>
+        <div className={styles.priceContainer}>
+          <p className={classNames(styles.price, discountPrice ? styles.priceInactive : '')}>
+            {currentPrice}
+            {currency}
+          </p>
+          {discountPrice ? (
+            <p className={styles.priceDiscount}>
+              {discountPrice}
+              {currency}
+            </p>
+          ) : (
+            ''
+          )}
+        </div>
+        <Button className={styles.button} variant="contained">
+          Add to cart
+          <ShoppingCartIcon />
+        </Button>
+      </div>
     </div>
-    <h2 className={styles.title}>{title}</h2>
-    <p className={styles.description}>{description}</p>
-    <div className={styles.priceContainer}>
-      <p className={classNames(styles.price, discountPrice ? styles.priceInactive : '')}>
-        {price}
-        {currency}
-      </p>
-      {discountPrice ? (
-        <p className={styles.priceDiscount}>
-          {discountPrice}
-          {currency}
-        </p>
-      ) : (
-        ''
-      )}
-    </div>
-    <Button className={styles.button} variant="contained">
-      Add to cart
-      <ShoppingCartIcon />
-    </Button>
   </div>
 );

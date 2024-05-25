f32d250cdad5a202f68ffb4f82a03b64bf847d12
Aleksei <kav.prok@gmail.com>
2024-05-25 15:35:44 +0700

    feat: navigate to 404 page if product slug does not exist

--- src/pages/product/product.page.tsx
+++ src/pages/product/product.page.tsx
@@ -1,25 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Product } from '@modules/product-page/product-page-module/product-page.module';
 import { useAppDispatch } from '@hooks/use-app-dispatch.hook';
 import { loadEnd, loading } from '@store/misc/misc.slice';
 import { clear, select } from '@store/product/product.slice';
-import { useLocation } from 'react-router-dom';
-import { useMountEffect } from '@hooks/use-mount-effect.hook';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { getProduct } from '@/API/product/product-adapter';
 // import styles from './product.page.module.scss';
 
 export const ProductPage: FC = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const location = useLocation().pathname.split('/');
   const slug = location[location.length - 1];
-  useMountEffect(() => {
+  useEffect(() => {
     dispatch(loading({}));
-  });
-  getProduct(slug)
-    .then(product => {
-      dispatch(select(product));
-    })
-    .catch(() => dispatch(clear()))
-    .finally(() => dispatch(loadEnd({})));
+    getProduct(slug)
+      .then(product => dispatch(select(product)))
+      .catch(() => {
+        navigate('/404', { replace: true });
+        dispatch(clear());
+      })
+      .finally(() => dispatch(loadEnd({})));
+  }, [slug]);
   return <Product />;
 };

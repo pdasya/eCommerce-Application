929ab3b037ac8f9b032bf5822fa3754fc5d8d0d7
Aleksei <kav.prok@gmail.com>
2024-05-25 23:12:03 +0700

    feat: install and configured swiper slider

--- src/modules/product-page/product-page-component/product-page.component.tsx
+++ src/modules/product-page/product-page-component/product-page.component.tsx
@@ -5,10 +5,14 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { List, ListItemButton, ListItemText, Collapse, Grid } from '@mui/material';
 import classNames from 'classnames';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import type { Swiper as SwiperType } from 'swiper';
+import { Navigation, Pagination, Thumbs } from 'swiper/modules';
 import { ISingleProduct } from '@/interfaces/interfaces';
 import { AttributeList } from '../component/product-attribute-list/product-attribute-list';
 import { AttributeCollapse } from '../component/product-attribute-collapse/product-attribute-collapse';
-import { ProductImage } from '../component/product-images/product-images';
+import { ProductImages } from '../component/product-images/product-images';
+import 'swiper/css/bundle';
 import styles from './product-page.component.module.scss';
 
 export const ProductItem: FC<ISingleProduct> = ({
@@ -21,6 +25,7 @@ export const ProductItem: FC<ISingleProduct> = ({
   discountPrice,
 }) => {
   const [openDescription, setOpenDescription] = useState(false);
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
   const handleClickDescription = () => {
     setOpenDescription(!openDescription);
   };
@@ -28,9 +33,47 @@ export const ProductItem: FC<ISingleProduct> = ({
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={6}>
-        {images.map(image => (
-          <ProductImage {...image} key={image.label} />
-        ))}
+        <Swiper
+          modules={[
+            Navigation,
+            Pagination,
+            Thumbs,
+          ]}
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation
+          pagination={{
+            clickable: true,
+          }}
+          thumbs={{
+            swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+            slideThumbActiveClass: styles.thumbActive,
+          }}>
+          {images.map(image => (
+            <SwiperSlide key={image.label} className={styles.slide}>
+              <ProductImages {...image} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Swiper
+          modules={[
+            Navigation,
+            Pagination,
+            Thumbs,
+          ]}
+          spaceBetween={10}
+          slidesPerView={3}
+          navigation
+          onSwiper={setThumbsSwiper}
+          style={{ marginTop: 10 }}>
+          {images.map(image => (
+            <SwiperSlide key={image.label} className={styles.slide}>
+              <div className={styles.thumb}>
+                <ProductImages {...image} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Grid>
       <Grid item xs={12} md={6}>
         <Grid className={styles.item} sx={{ p: 2 }}>

c64108eac83045dc354381baa7e828f510c4ff43
Tim <jasper7466@yandex.ru>
2024-05-26 16:45:20 +0500

    feat: update catalog slice

--- src/store/catalog/catalog.slice.ts
+++ src/store/catalog/catalog.slice.ts
@@ -1,22 +1,66 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { SortBy } from '@config/sorting-options';
 import { RootState } from '@/store';
 import { IProduct } from '@/interfaces/interfaces';
 
 interface ICatalogState {
-  sort: string;
+  sort: SortBy;
   products: IProduct[];
+  category: string | null;
+  priceLimits: { min: number; max: number };
+  priceFilter: { min: number; max: number };
+  customFilterOptions: Record<string, string[]>;
+  customFilters: Record<string, Record<string, boolean>>;
 }
 
 const initialState: ICatalogState = {
-  sort: 'createdAt asc',
+  sort: SortBy.default,
   products: [],
+  category: null,
+  priceLimits: { min: 0, max: 0 },
+  priceFilter: { min: 0, max: 0 },
+  customFilterOptions: {},
+  customFilters: {},
 };
 
 const catalogSlice = createSlice({
   name: 'catalog',
   initialState,
   reducers: {
-    sort(state, action) {
+    setCategory(state, action: PayloadAction<ICatalogState['category']>) {
+      state.category = action.payload;
+    },
+    setPriceLimits(state, action: PayloadAction<ICatalogState['priceLimits']>) {
+      state.priceLimits = action.payload;
+    },
+    setPriceFilter(state, action: PayloadAction<ICatalogState['priceFilter']>) {
+      state.priceFilter = action.payload;
+    },
+    setCustomFilterOptions(state, action: PayloadAction<ICatalogState['customFilterOptions']>) {
+      const initialEntries = Object.entries(action.payload).map(([attribute, values]) => [
+        attribute,
+        values.reduce((acc, option) => ({ ...acc, [option]: false }), {}),
+      ]);
+
+      state.customFilters = Object.fromEntries(initialEntries);
+      state.customFilterOptions = { ...action.payload };
+    },
+    setCustomFilters(state, action: PayloadAction<ICatalogState['customFilters']>) {
+      state.customFilters = { ...action.payload };
+    },
+    resetAllFilters(state, action: PayloadAction<void>) {
+      const initialEntries = Object.entries(state.customFilterOptions).map(
+        ([attribute, values]) => [
+          attribute,
+          values.reduce((acc, option) => ({ ...acc, [option]: false }), {}),
+        ],
+      );
+
+      state.customFilters = Object.fromEntries(initialEntries);
+      const { min, max } = state.priceLimits;
+      state.priceFilter = { min, max };
+    },
+    sort(state, action: PayloadAction<ICatalogState['sort']>) {
       state.sort = action.payload;
     },
     update(state, action: PayloadAction<IProduct[]>) {
@@ -28,7 +72,21 @@ const catalogSlice = createSlice({
   },
 });
 
-export const { sort, update, clear } = catalogSlice.actions;
+export const {
+  sort,
+  update,
+  clear,
+  setPriceFilter,
+  setPriceLimits,
+  setCustomFilterOptions,
+  setCustomFilters,
+  resetAllFilters,
+} = catalogSlice.actions;
 export const selectProducts = (state: RootState) => state.catalog.products;
 export const selectSort = (state: RootState) => state.catalog.sort;
+export const selectCategory = (state: RootState) => state.catalog.category;
+export const selectPriceLimits = (state: RootState) => state.catalog.priceLimits;
+export const selectPriceFilter = (state: RootState) => state.catalog.priceFilter;
+export const selectCustomFilterOptions = (state: RootState) => state.catalog.customFilterOptions;
+export const selectCustomFilters = (state: RootState) => state.catalog.customFilters;
 export const catalogReducer = catalogSlice.reducer;

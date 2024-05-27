8b5f57355c5baf518a14179d3b2c8754be5c375d
Tim <jasper7466@yandex.ru>
2024-05-28 01:36:00 +0500

    feat: implement get categories api requests

---
+++ src/API/categories/get-categories.ts
@@ -0,0 +1,37 @@
+import { CategoryPagedQueryResponse, ClientResponse } from '@commercetools/platform-sdk';
+import { client } from '@config/constants';
+import { fetchAllDecorator } from '../utils/fetch-all.decorator';
+import { ICategory } from '@/interfaces/category.interface';
+
+const getAllTopLevelCategoriesRequest = fetchAllDecorator(async options =>
+  client
+    .getClient()
+    .categories()
+    .get({ queryArgs: { ...options, where: 'parent is not defined' } })
+    .execute(),
+);
+
+const getAllCategoriesRequest = fetchAllDecorator(async options =>
+  client
+    .getClient()
+    .categories()
+    .get({ queryArgs: { ...options } })
+    .execute(),
+);
+
+const getCategoriesResponseAdapter = (
+  response: ClientResponse<CategoryPagedQueryResponse>,
+  locale = 'en',
+): ICategory[] =>
+  response.body.results.map(({ id, name, description, slug }) => ({
+    id,
+    caption: name[locale],
+    description: description ? description[locale] : '',
+    slug: slug[locale],
+  }));
+
+export const getAllCategories = async () =>
+  getCategoriesResponseAdapter(await getAllCategoriesRequest());
+
+export const getAllTopLevelCategories = async () =>
+  getCategoriesResponseAdapter(await getAllTopLevelCategoriesRequest());
---
+++ src/interfaces/category.interface.ts
@@ -0,0 +1,6 @@
+export interface ICategory {
+  id: string;
+  caption: string;
+  slug: string;
+  description: string;
+}

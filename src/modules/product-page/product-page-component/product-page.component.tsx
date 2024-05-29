cb2a4cdf27767f7f9fbe752ed28ec2771d661caa
Aleksei <kav.prok@gmail.com>
2024-05-29 10:56:59 +0700

    fix: fix slider jerking

--- src/modules/product-page/product-page-component/product-page.component.tsx
+++ src/modules/product-page/product-page-component/product-page.component.tsx
@@ -1,13 +1,12 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { ExpandLess, ExpandMore } from '@mui/icons-material';
-import { List, ListItemButton, ListItemText, Collapse, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import classNames from 'classnames';
 import { ISingleProduct } from '@/interfaces/interfaces';
 import { AttributeList } from '../component/product-attribute-list/product-attribute-list';
-import { AttributeCollapse } from '../component/product-attribute-collapse/product-attribute-collapse';
+import { ProductCollapse } from '../component/product-collapse/product-collapse';
 import styles from './product-page.component.module.scss';
 import { Gallery } from '../component/product-gallery/product-gallery.component';
 
@@ -19,70 +18,46 @@ export const ProductItem: FC<ISingleProduct> = ({
   currency,
   attributes,
   discountPrice,
-}) => {
-  const [openDescription, setOpenDescription] = useState(false);
-  const handleClickDescription = () => {
-    setOpenDescription(!openDescription);
-  };
-
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <Gallery images={images} />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Grid className={styles.item} sx={{ p: 2 }}>
-          <Typography variant="h4" className={styles.title}>
-            {title}
-          </Typography>
-          <div>
-            {attributes.map(attribute => (
-              <AttributeList {...attribute} key={attribute.name} />
-            ))}
-          </div>
-          <div className={styles.priceContainer}>
-            <p className={classNames(styles.price, discountPrice ? styles.priceInactive : '')}>
-              {currentPrice}
+}) => (
+  <Grid container spacing={2}>
+    <Grid item xs={12} md={6}>
+      <Gallery images={images} />
+    </Grid>
+    <Grid item xs={12} md={6}>
+      <Grid className={styles.item} sx={{ p: 2 }}>
+        <Typography variant="h4" className={styles.title}>
+          {title}
+        </Typography>
+        <div>
+          {attributes.map(attribute => (
+            <AttributeList {...attribute} key={attribute.name} />
+          ))}
+        </div>
+        <div className={styles.priceContainer}>
+          <p className={classNames(styles.price, discountPrice ? styles.priceInactive : '')}>
+            {currentPrice}
+            {currency}
+          </p>
+          {discountPrice ? (
+            <p className={styles.priceDiscount}>
+              {discountPrice}
               {currency}
             </p>
-            {discountPrice ? (
-              <p className={styles.priceDiscount}>
-                {discountPrice}
-                {currency}
-              </p>
-            ) : (
-              ''
-            )}
-          </div>
-          <Button className={styles.button} variant="contained">
-            Add to cart
-            <ShoppingCartIcon />
-          </Button>
-        </Grid>
-        <Grid item className={styles.list}>
-          {description ? (
-            <List
-              sx={{ bgcolor: 'background.paper' }}
-              component="nav"
-              aria-labelledby="nested-list-subheader">
-              <ListItemButton onClick={handleClickDescription}>
-                <ListItemText primary="Description" />
-                {openDescription ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-              <Collapse in={openDescription} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                  <ListItemText sx={{ pl: 2, pr: 2 }} primary={description} />
-                </List>
-              </Collapse>
-            </List>
           ) : (
             ''
           )}
-          {attributes.map(attribute => (
-            <AttributeCollapse {...attribute} key={attribute.name} />
-          ))}
-        </Grid>
+        </div>
+        <Button className={styles.button} variant="contained">
+          Add to cart
+          <ShoppingCartIcon />
+        </Button>
+      </Grid>
+      <Grid item className={styles.list}>
+        {description ? <ProductCollapse {...{ name: 'description', value: description }} /> : ''}
+        {attributes.map(attribute => (
+          <ProductCollapse {...attribute} key={attribute.name} />
+        ))}
       </Grid>
     </Grid>
-  );
-};
+  </Grid>
+);

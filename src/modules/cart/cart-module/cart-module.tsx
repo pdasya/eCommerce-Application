f3ba0712753486585a558b7058282f37a9d457e4
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-06-10 11:16:45 +0900

    refactor: move API interactions to separate file

--- src/modules/cart/cart-module/cart-module.tsx
+++ src/modules/cart/cart-module/cart-module.tsx
@@ -1,10 +1,16 @@
 import { Button, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { FC, useState } from 'react';
-import { client } from '@config/constants';
-import { CartUpdateAction } from '@commercetools/platform-sdk';
+// import { client } from '@config/constants';
+// import { CartUpdateAction } from '@commercetools/platform-sdk';
 import { toast } from 'react-toastify';
-import { getUserCart } from '../cart-module-api/cart-module-api';
+import {
+  fetchCart,
+  handleCartClear,
+  handleDelete,
+  handlePromoCodeApply,
+  handleQuantityChange,
+} from '../cart-module-api/cart-module-api';
 import styles from './cart-module.module.scss';
 import { CartHeader } from '../components/cart-header/cart-header';
 import { CartItemList } from '../components/cart-item-list/cart-item-list';
@@ -28,17 +34,10 @@ export const CartModule: FC = () => {
   const [promoCode, setPromoCode] = useState('');
   const [promoError, setPromoError] = useState('');
 
-  const fetchCart = async () => {
+  const fetchCartItems = async () => {
     setLoading(true);
     try {
-      const cart = await getUserCart();
-      const items = cart.lineItems.map(item => ({
-        id: item.id,
-        name: item.name.en,
-        imageUrl: 'https://via.placeholder.com/150',
-        price: item.price.value.centAmount / 100,
-        quantity: item.quantity,
-      }));
+      const items = await fetchCart();
       setCartItems(items);
       setLoading(false);
     } catch (error) {
@@ -47,80 +46,20 @@ export const CartModule: FC = () => {
     }
   };
 
-  const handleDelete = async (id: string) => {
-    const updateActionDelete = [
-      {
-        action: 'removeLineItem',
-        lineItemId: id,
-      },
-    ];
-
+  const handleDeleteItem = async (id: string) => {
     try {
-      const response = await client.getClient().me().activeCart().get().execute();
-      const cartVersion = response.body.version;
-      const currentCartId = response.body.id;
-
-      const itemName =
-        response.body.lineItems.find((item: { id: string }) => item.id === id)?.name?.en || 'Item';
-
-      await client
-        .getClient()
-        .carts()
-        .withId({ ID: currentCartId })
-        .post({
-          body: {
-            version: cartVersion,
-            actions: updateActionDelete as CartUpdateAction[],
-          },
-        })
-        .execute();
-
+      const itemName = await handleDelete(id);
       setCartItems(prevItems => prevItems.filter(item => item.id !== id));
-
-      const cartResponse = await client.getClient().me().carts().get().execute();
-
-      // Для проверки работы функционала добавления
-      console.log(cartResponse.body.results);
       toast.success(`Item ${itemName} is successfully deleted from the cart`);
     } catch (error) {
-      console.error('Error updating cart:', error);
       toast.error(`Error deleting item`);
     }
   };
 
-  const handleQuantityChange = async (id: string, newQuantity: number) => {
-    const updateQuantityAction = [
-      {
-        action: 'changeLineItemQuantity',
-        lineItemId: id,
-        quantity: newQuantity,
-      },
-    ];
-
+  const handleQuantityChangeItem = async (id: string, newQuantity: number) => {
     try {
-      const response = await client.getClient().me().activeCart().get().execute();
-      const cartVersion = response.body.version;
-      const currentCartId = response.body.id;
-
-      const itemName =
-        response.body.lineItems.find((item: { id: string }) => item.id === id)?.name?.en || 'Item';
-
-      await client
-        .getClient()
-        .carts()
-        .withId({ ID: currentCartId })
-        .post({
-          body: {
-            version: cartVersion,
-            actions: updateQuantityAction as CartUpdateAction[],
-          },
-        })
-        .execute();
+      const itemName = await handleQuantityChange(id, newQuantity);
       toast.success(`Item ${itemName} quantity successfully changed`);
-
-      // Для проверки работы функционала изменения количества
-      const responseCheck = await client.getClient().me().activeCart().get().execute();
-      console.log(responseCheck.body);
     } catch (error) {
       console.error(`Error updating quantity: ${error}`);
     }
@@ -131,7 +70,7 @@ export const CartModule: FC = () => {
       prevItems.map(item => {
         if (item.id === id) {
           const newQuantity = item.quantity + 1;
-          handleQuantityChange(id, newQuantity);
+          handleQuantityChangeItem(id, newQuantity);
           return { ...item, quantity: newQuantity };
         }
         return item;
@@ -144,48 +83,23 @@ export const CartModule: FC = () => {
       prevItems.map(item => {
         if (item.id === id && item.quantity > 1) {
           const newQuantity = item.quantity - 1;
-          handleQuantityChange(id, newQuantity);
+          handleQuantityChangeItem(id, newQuantity);
           return { ...item, quantity: newQuantity };
         }
         if (item.quantity === 1 && item.id === id) {
-          handleDelete(id);
+          handleDeleteItem(id);
         }
         return item;
       }),
     );
   };
 
-  const handleCartClear = async () => {
-    const clearCartActions = cartItems.map(item => ({
-      action: 'removeLineItem',
-      lineItemId: item.id,
-    }));
-
+  const handleCartClearItems = async () => {
     try {
-      const response = await client.getClient().me().activeCart().get().execute();
-      const cartVersion = response.body.version;
-      const currentCartId = response.body.id;
-
-      await client
-        .getClient()
-        .carts()
-        .withId({ ID: currentCartId })
-        .post({
-          body: {
-            version: cartVersion,
-            actions: clearCartActions as CartUpdateAction[],
-          },
-        })
-        .execute();
-
+      await handleCartClear(cartItems);
       setCartItems([]);
       toast.success('Cart has been cleared');
-
-      // Для проверки работы функционала удаления товаров из корзины
-      const responseCheck = await client.getClient().me().activeCart().get().execute();
-      console.log(responseCheck.body);
     } catch (error) {
-      console.error('Error clearing cart:', error);
       toast.error('Error clearing cart');
     }
   };
@@ -201,66 +115,17 @@ export const CartModule: FC = () => {
     }
   };
 
-  const handlePromoCodeApply = async () => {
-    if (!VALID_PROMO_CODES.includes(promoCode)) {
-      setPromoError('Invalid promo code');
-      return;
-    }
-
-    const updatePromoCodeActions = [
-      {
-        action: 'addDiscountCode',
-        code: promoCode,
-      },
-    ];
-
+  const handlePromoCodeApplyItems = async () => {
     try {
-      const response = await client.getClient().me().activeCart().get().execute();
-      const cartVersion = response.body.version;
-      const currentCartId = response.body.id;
-
-      await client
-        .getClient()
-        .carts()
-        .withId({ ID: currentCartId })
-        .post({
-          body: {
-            version: cartVersion,
-            actions: updatePromoCodeActions as CartUpdateAction[],
-          },
-        })
-        .execute();
-
-      const cartResponse = await client.getClient().me().carts().get().execute();
-
-      const items = cartResponse.body.results[0].lineItems.map(item => {
-        const discountedPrice = item.price.discounted?.value.centAmount;
-        const regularPrice = item.price.value.centAmount;
-
-        return {
-          id: item.id,
-          name: item.name.en,
-          imageUrl: 'https://via.placeholder.com/150',
-          price: discountedPrice ? discountedPrice / 100 : regularPrice / 100,
-          oldPrice: discountedPrice ? regularPrice / 100 : undefined,
-          quantity: item.quantity,
-        };
-      });
-
+      const items = await handlePromoCodeApply(promoCode, VALID_PROMO_CODES);
       setCartItems(items);
-      // Для проверки работы функционала работы промокода
-      console.log(cartResponse.body.results);
       toast.success(`Promocode ${promoCode} is successfully applied`);
       setPromoCode('');
     } catch (error) {
-      console.error('Error applying promocode:', error);
-      toast.error(`Error applying promocode:`);
+      setPromoError('Invalid promo code');
+      toast.error(`Error applying promo code: ${error.message}`);
     }
   };
-
-  // useEffect(() => {
-  //   fetchCart();
-  // }, []);
 
   return (
     <Grid container spacing={2} className={styles.cartModuleWrapper}>
@@ -281,7 +146,7 @@ export const CartModule: FC = () => {
                 cartItems={cartItems}
                 handleAdd={handleAdd}
                 handleRemove={handleRemove}
-                handleDelete={handleDelete}
+                handleDelete={handleDeleteItem}
               />
               <Box textAlign="right" marginTop={2}>
                 <Typography variant="h6">
@@ -295,13 +160,13 @@ export const CartModule: FC = () => {
                 promoCode={promoCode}
                 promoError={promoError}
                 handlePromoCodeChange={handlePromoCodeChange}
-                handlePromoCodeApply={handlePromoCodeApply}
+                handlePromoCodeApply={handlePromoCodeApplyItems}
               />
-              <CartActions handleCartClear={handleCartClear} />
+              <CartActions handleCartClear={handleCartClearItems} />
             </>
           )}
           {cartItems.length === 0 && (
-            <Button variant="contained" color="info" onClick={fetchCart}>
+            <Button variant="contained" color="info" onClick={fetchCartItems}>
               Add Cart Items
             </Button>
           )}

2ad2a43e2da25494a835fa2982726eb1ad941afc
Aleksei <kav.prok@gmail.com>
2024-06-12 17:57:25 +0700

    refactor: add impact info

--- src/modules/about/about-module/about-module.tsx
+++ src/modules/about/about-module/about-module.tsx
@@ -23,31 +23,31 @@ export const AboutContent: FC = () => {
     {
       name: 'Aleksei',
       role: 'Frontend Developer',
-      bio: "My name is Aleksei, I'm 35 years old and I'm a front-end developer. My journey in web development began with self-study in 2017. To this day, I continue my journey as a web developer, and every day is an opportunity for me to learn something new and improve my skills.",
+      bio: "I'm a front-end developer. My journey in web development began with self-study in 2017. To this day, I continue my journey as a web developer, and every day is an opportunity for me to learn something new and improve my skills.",
       photoUrl: './assets/images/photo/aleksei.jpg',
       githubLink: 'https://github.com/nicealx',
     },
   ];
   return (
-    <>
-      <Typography variant="h1" component="h1" className={styles.title}>
+    <Grid>
+      <Typography variant="h1" component="h1" className={styles.aboutTitle}>
         About Us
       </Typography>
-      <Box sx={{ textAlign: 'center', maxWidth: 800, margin: '0 auto' }}>
-        <p>
+      <Box className={styles.about}>
+        <Typography variant="body1" sx={{ marginTop: 1 }}>
           We&apos;re a team of web developers, united by a common goal: to create an e-Commerce
           application. This project is not only our first experience of collaboration but also our
           first try into React development.
-        </p>
-        <p>
+        </Typography>
+        <Typography variant="body1" sx={{ marginTop: 1 }}>
           Our journey into React development has been both challenging and rewarding. As we dove
           into the intricacies of React components and hooks, we found ourselves continuously
           learning and adapting to new methodologies. Also, this project has allowed us to
           experiment with state management techniques, pushing our development skills to new
           heights.
-        </p>
+        </Typography>
       </Box>
-      <Box sx={{ textAlign: 'center', maxWidth: 800, margin: '20px auto' }}>
+      <Box className={styles.title}>
         <Typography variant="h5" component="h5">
           Team Members
         </Typography>
@@ -59,6 +59,29 @@ export const AboutContent: FC = () => {
           </Grid>
         ))}
       </Grid>
-    </>
+      <Grid>
+        <Box className={styles.title}>
+          <Typography variant="h5" component="h5">
+            Impact of collaboration
+          </Typography>
+        </Box>
+        <Box className={styles.about}>
+          <Typography variant="body1" sx={{ marginTop: 1 }}>
+            Although we three were strangers at the start of the project, this did not prevent us
+            from quickly establishing a collaborative rapport. We quickly recognized each
+            other&apos;s strengths and weaknesses, which helped us navigate through all the
+            challenges of team development. The toughest part of the project involved grappling with
+            the Commercetools platform and its SDK, often joking about the complexity and
+            unpredictability of its documentation. However, over each iteration we developed more
+            suitable and reusable solutions. This experience boosted our confidence in using Git and
+            code review. You can check out the code for our application at the provided link.
+          </Typography>
+          <Typography variant="body1" sx={{ marginTop: 1 }}>
+            We welcome and appreciate different viewpoints, so if you have any feedback or comments,
+            please share them with us.
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };

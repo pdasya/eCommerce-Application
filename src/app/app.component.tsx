6968ffe19b2483b0bfaa1aa5f566599f2717a110
Tim <jasper7466@yandex.ru>
2024-06-16 23:44:33 +0500

    fix: update cart on auth state change

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -4,22 +4,35 @@ import { Outlet } from 'react-router-dom';
 import { GlobalLoading } from '@components/global-loading/global-loading.component';
 import 'react-toastify/dist/ReactToastify.css';
 import { cartService } from '@modules/cart';
+import { useAppSelector } from '@hooks/use-app-selector.hook';
+import { selectAuthorization } from '@store/auth/auth.slice';
 import { Header } from '@/modules/header';
 import { Footer } from '@/modules/footer';
 import { authService } from '@/services/auth.service';
 import * as styles from './app.component.module.scss';
 
 const App = (): ReactElement => {
+  const isAuthorized = useAppSelector(selectAuthorization);
+
   useEffect(() => {
     (async () => {
       try {
         await authService.init();
-        await cartService.synchronize();
       } catch (error) {
-        toast.error(error);
+        toast.error(error.message);
       }
     })();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await cartService.synchronize();
+      } catch (error) {
+        toast.error(error.message);
+      }
+    })();
+  }, [isAuthorized]);
 
   return (
     <div className={styles.app}>

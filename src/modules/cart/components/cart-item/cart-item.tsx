aab72d0ee87253f89616614ae0c8f5f91689b83b
Tim <jasper7466@yandex.ru>
2024-06-17 13:45:48 +0500

    fix: block cart item ui while perform api requests
    # Conflicts:
    #	src/modules/cart/module/cart-module.component.tsx

--- src/modules/cart/components/cart-item/cart-item.tsx
+++ src/modules/cart/components/cart-item/cart-item.tsx
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CardMedia, Typography, IconButton, Box, CardContent, Card } from '@mui/material';
 import { Delete, Add, Remove } from '@mui/icons-material';
 import { CustomRouterLink } from '@components/custom-router-link/custom-router-link.component';
 import { RoutePath } from '@routes/index';
 import { generatePath } from 'react-router-dom';
+import { FullSizeLoading } from '@components/fullsize-loading/full-size-loading.component';
+import { cartService } from '@modules/cart/services/cart.service';
+import { toast } from 'react-toastify';
 import * as styles from './cart-item.module.scss';
 
 interface CartItemProps {
+  id: string;
   name: string;
   slug: string;
   imageUrl: string;
   initialPrice: number;
   finalPrice: number;
   quantity: number;
-  onIncrement: () => void;
-  onDecrement: () => void;
-  onRemove: () => void;
 }
 
 const CartItem: React.FC<CartItemProps> = ({
+  id,
   name,
   slug,
   imageUrl,
   initialPrice,
   finalPrice,
   quantity,
-  onIncrement,
-  onDecrement,
-  onRemove,
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const totalPrice = finalPrice * quantity;
+
+  const removeHandler = async () => {
+    setIsLoading(true);
+    try {
+      await cartService.removeCartItem({ id });
+      toast.success(`Item ${name} is successfully deleted from the cart`);
+    } catch (error) {
+      toast.error(`Error deleting item`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const incrementHandler = async () => {
+    setIsLoading(true);
+    try {
+      await cartService.updateCartItemQuantity([{ id, quantity: quantity + 1 }]);
+    } catch (error) {
+      toast.error(`Error updating quantity: ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const decrementHandler = async () => {
+    setIsLoading(true);
+    try {
+      await cartService.updateCartItemQuantity([{ id, quantity: quantity - 1 }]);
+    } catch (error) {
+      toast.error(`Error updating quantity: ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Card className={styles.cartItem}>
+      <FullSizeLoading isLoading={isLoading} />
       <CardMedia component="img" className={styles.cartItemMedia} image={imageUrl} alt={name} />
       <CardContent className={styles.cartItemContent}>
         <CustomRouterLink
@@ -59,18 +94,18 @@ const CartItem: React.FC<CartItemProps> = ({
           Quantity
         </Typography>
         <Box className={styles.cartItemBoxQuantity}>
-          <IconButton onClick={() => onDecrement()}>
+          <IconButton onClick={decrementHandler}>
             <Remove />
           </IconButton>
           <Typography className={styles.cartItemBoxQuantityText}>{quantity}</Typography>
-          <IconButton onClick={() => onIncrement()}>
+          <IconButton onClick={incrementHandler}>
             <Add />
           </IconButton>
         </Box>
       </Box>
       <CardContent className={styles.cartItemTotalPrice}>
         <Typography variant="h6">${totalPrice.toFixed(2)}</Typography>
-        <IconButton onClick={() => onRemove()} color="error" className={styles.deleteButton}>
+        <IconButton onClick={removeHandler} color="error" className={styles.deleteButton}>
           <Delete />
         </IconButton>
       </CardContent>
